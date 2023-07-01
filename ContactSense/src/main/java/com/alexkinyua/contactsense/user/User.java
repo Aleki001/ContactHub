bb4eed29b200cd7b4f375a38ebd907364cccacdf
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-07-01 12:47:42 +0300

    added dependancies to the project and created entity user

---
+++ ContactSense/src/main/java/com/alexkinyua/contactsense/user/User.java
@@ -0,0 +1,44 @@
+package com.alexkinyua.contactsense.user;
+
+import com.alexkinyua.contactsense.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
+
+import java.util.Collection;
+
+/**
+  * @Author: Alex Kinyua
+  */
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @NaturalId(mutable = true)
+    private String email;
+    private String password;
+    private boolean isEnabled = false;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Collection<Role> roles;
+
+
+    public User(String name, String email, String password, Collection<Role> roles) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+}
---
+++ ContactSense/src/test/java/com/alexkinyua/contactsense/ContactSenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexkinyua.contactsense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactSenseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
